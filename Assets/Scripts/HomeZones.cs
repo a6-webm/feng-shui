e74f817e8a7f78d189e3ea2d14ad76440a873368
a6.webm <46289515+a6-webm@users.noreply.github.com>
2023-06-05 21:22:46 +0100

    add some scripts and an empty gameobject

---
+++ Assets/Scripts/HomeZones.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HomeZones : MonoBehaviour
+{
+    [SerializeField]
+    List<List<GameObject>> zonesList;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach (List<GameObject> path in zonesList) {
+            
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
