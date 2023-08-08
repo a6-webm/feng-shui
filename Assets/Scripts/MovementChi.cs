e72f4b0990569858c0f89ca851dbcbf4b5c1eccd
a6.webm <46289515+a6-webm@users.noreply.github.com>
2023-08-08 13:14:55 +0100

    finish chi line smoothing
    Update MovementChi.cs

--- Assets/Scripts/MovementChi.cs
+++ Assets/Scripts/MovementChi.cs
@@ -4,21 +4,26 @@ using UnityEngine;
 using UnityEngine.AI;
 using static UnityEngine.AI.NavMeshPathStatus;
 
-[RequireComponent(typeof(Edge))]
+[RequireComponent(typeof(Edge), typeof(LineRenderer))]
 public class MovementChi : MonoBehaviour
 {
-    [SerializeField] float LineSmoothness = 1.3f;
+    [SerializeField] float LineSmoothness = 2.18f;
+    [SerializeField] float LineSmoothnessScaling = 0.25f;
     [SerializeField] float LineResolution = 3f;
 
     private Edge _edge;
     private NavMeshPath _navMeshPath;
-    private List<Vector3> _smoothLine;
     private LineRenderer _lineRenderer;
 
     void Start()
     {
         _edge = GetComponent<Edge>();
-        // _lineRenderer = GetComponent<LineRenderer>();
+        _lineRenderer = GetComponent<LineRenderer>();
+
+        transform.rotation = Quaternion.Euler(90f, 0f, 0f);
+        _lineRenderer.numCapVertices = 5;
+        _lineRenderer.alignment = LineAlignment.TransformZ;
+        
     }
 
     void FixedUpdate() {
@@ -26,16 +31,15 @@ public class MovementChi : MonoBehaviour
         Vector3 ptB = _edge.points()[1];
         _navMeshPath = new();
         NavMesh.CalculatePath(ptA, ptB, NavMesh.AllAreas, _navMeshPath);
-        _smoothLine = smoothLine(_navMeshPath.corners);
+        var smooth = smoothLine(_navMeshPath.corners);
+        _lineRenderer.positionCount = smooth.Count;
+        _lineRenderer.SetPositions(smooth.ToArray());
     }
 
     private void OnDrawGizmos() {
         List<Vector3> points = GetComponent<Edge>().points();
-        if (_navMeshPath != null && _smoothLine != null) {
+        if (_navMeshPath != null) {
             Gizmos.color = _navMeshPath.status == PathComplete ? Color.yellow : Color.red;
-            Gizmos.DrawLineStrip(_smoothLine.ToArray(), false);
-            _smoothLine.ForEach(p => Gizmos.DrawSphere(p, 0.1f));
-            Gizmos.color = Color.red;
             Gizmos.DrawLineStrip(_navMeshPath.corners, false);
             foreach (var p in _navMeshPath.corners) { Gizmos.DrawSphere(p, 0.2f); }
         }
@@ -52,7 +56,7 @@ public class MovementChi : MonoBehaviour
             return new List<Vector3>(knots);
         }
         List<Vector3> smoothLine = new();
-        Vector3 outCtrl = (knots[1] - knots[0]).normalized * LineSmoothness;
+        Vector3 outCtrl = (knots[1] - knots[0]).normalized;
         for (int i = 0; i < knots.Length - 2; i++) {
             Vector3 pt = knots[i];
             Vector3 nextPt = knots[i+1];
@@ -60,14 +64,13 @@ public class MovementChi : MonoBehaviour
 
             var a = pt - nextPt;
             var b = nextNextPt - nextPt;
-            Vector3 nextInCtrl = ((pt - nextPt).normalized - (nextNextPt - nextPt).normalized).normalized
-                * LineSmoothness;
+            Vector3 nextInCtrl = ((pt - nextPt).normalized - (nextNextPt - nextPt).normalized).normalized;
 
             addCurve(
                 ref smoothLine,
                 pt,
-                pt + outCtrl,
-                nextPt + nextInCtrl,
+                pt + outCtrl * sigmoidAt0((pt - nextPt).magnitude, LineSmoothness, LineSmoothnessScaling),
+                nextPt + nextInCtrl * sigmoidAt0((pt - nextPt).magnitude, LineSmoothness, LineSmoothnessScaling),
                 nextPt
             );
 
@@ -76,8 +79,8 @@ public class MovementChi : MonoBehaviour
         addCurve(
             ref smoothLine,
             knots[^2],
-            knots[^2] + outCtrl,
-            knots[^1] + (knots[^2] - knots[^1]).normalized * LineSmoothness,
+            knots[^2] + outCtrl * sigmoidAt0((knots[^2] - knots[^1]).magnitude, LineSmoothness, LineSmoothnessScaling),
+            knots[^1] + (knots[^2] - knots[^1]).normalized * sigmoidAt0((knots[^2] - knots[^1]).magnitude, LineSmoothness, LineSmoothnessScaling),
             knots[^1]
         );
         smoothLine.Add(knots[^1]);
@@ -99,5 +102,9 @@ public class MovementChi : MonoBehaviour
              + p1 * ( 3*Mathf.Pow(t, 3) - 6*Mathf.Pow(t, 2) + 3*t)
              + p2 * (-3*Mathf.Pow(t, 3) + 3*Mathf.Pow(t, 2))
              + p3 *     Mathf.Pow(t, 3);
+    }
+
+    private float sigmoidAt0(float x, float height, float pitch) {
+        return height * (2f / (1f + Mathf.Exp(-pitch*x)) - 1f);
     }
 }
